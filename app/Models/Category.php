ec8f90b59b188b1396dabdf547ce2b4881c314d9
summercake@qq.com <summercake@qq.com>
2018-07-21 07:42:34 +0000

    initialize admin backend

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
+    protected $table = 'categories';
     protected $fillable = [
         'pid','sid','name'
     ];
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $table = 'posts';
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
 
     public function category()
